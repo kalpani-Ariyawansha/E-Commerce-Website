fe41a74fded0c588af750abec06230e95ef848c2
kavishka <thushalrasingolla99@gmail.com>
2024-07-26 11:20:56 +0530

    error mssg done

--- Frontend/techx/src/pages/Products/Products.jsx
+++ Frontend/techx/src/pages/Products/Products.jsx
@@ -8,9 +8,17 @@ function Products() {
   
   const {category}=useParams()
   
+  const error=()=>{
+    if(ItemData?.filter((item)=>(item.category.toLowerCase()===category.toLowerCase())).length===0){
+      return <div className='text-2xl text-[#25AEFF] font-semibold text-center'>No products found</div>
+    }
+    
+  }
+
   return (
     <div className='flex gap-9 mx-auto justify-start mt-10 mb-10 flex-wrap max-w-screen-xl'>
-      {ItemData?.filter((item)=>(item.category===category)).map((item,i)=>(item.items.map((item,i)=>(<PopularCard item={item} key={i}/>))) )}
+      {error()}
+      {ItemData?.filter((item)=>(item.category.toLowerCase()===category.toLowerCase())).map((item,i)=>(item.items.map((item,i)=>(<PopularCard item={item} key={i}/>))) )}
         
        
     </div>
